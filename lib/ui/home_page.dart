d4cf2e7b57b137f0f00f993977611f0b3da1f197
Eduard-Dumitrescu <eduard.dumitrescu94@gmail.com>
2020-03-28 23:06:28 +0200

    added firebase

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -5,7 +5,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Where youa at"),
+        title: const Text("Where youa at buddy"),
       ),
       body: Container(),
     );
